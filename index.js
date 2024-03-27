471bb89233d5d7907b5333a3aa9765fbf94b4bf5
Mirza Nauman Baig <Mirza.Baig@oma.org>
2024-03-27 12:53:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+let num1 = await inquirer.prompt([
+    {
+        name: "value",
+        message: "enter your operand1 :",
+        type: "input"
+    }
+]);
+let num2 = await inquirer.prompt([
+    {
+        name: "value",
+        message: "enter your operand2 :",
+        type: "input"
+    }
+]);
+let op = await inquirer.prompt([
+    {
+        name: "value",
+        message: "enter your operator :",
+        type: "input"
+    }
+]);
+if (op.value == "+") {
+    console.log(num1.value + " + " + num2.value + " = " + (parseFloat(num1.value) + parseFloat(num2.value)));
+}
+else if (op.value == "-") {
+    console.log(num1.value + " - " + num2.value + " = " + (parseFloat(num1.value) - parseFloat(num2.value)));
+}
+else if (op.value == "*") {
+    console.log(num1.value + " * " + num2.value + " = " + (parseFloat(num1.value) * parseFloat(num2.value)));
+}
+else if (op.value == "/") {
+    console.log(num1.value + " / " + num2.value + " = " + (parseFloat(num1.value) / parseFloat(num2.value)));
+}
+else if (op.value == "%") {
+    console.log(num1.value + " % " + num2.value + " = " + (parseFloat(num1.value) % parseFloat(num2.value)));
+}
+else {
+    console.log("Invalid input");
+}
